86d01bdc09733c9890f859a2c29557f8ac4f2609
zhoubo02 <zhoubo02@hnnewsoft.com>
2021-11-24 16:25:20 +0800

    add db table and struct

--- types/structs.go
+++ types/structs.go
@@ -11,11 +11,41 @@ type Base struct {
 type BaseTask struct {
 	State int
 	Message string
+	params string // used for create sub tasks
 }
 
 
 type PartReBalanceTask struct {
 	*Base
 	*BaseTask
-	params string // used for create sub tasks
+}
+
+type TransferTask struct {
+	*Base
+	*BaseTask
+	RebalanceId  uint64 `xorm:"rebalance_id"`
+	TransferType uint8  `xorm:"transfer_type"`
+	Progress     string `xorm:"progress"`
+}
+
+
+type TransactionTask struct {
+	*Base
+	*BaseTask
+	RebalanceId     uint64 `xorm:"rebalance_id"`
+	TransferId      uint64 `xorm:"transfer_id"`
+	Nonce           int    `xorm:"nonce"`
+	ChainId         int    `xorm:"chain_id"`
+	From            string `xorm:"from"`
+	To              string `xorm:"to"`
+	ContractAddress string `xorm:"contract_address"`
+	Value           int    `xorm:"value"`
+	UnSignData      string `xorm:"unsigned_data"`
+	SignData        string `xorm:"signed_data"`
+}
+
+type InvestTask struct {
+	*Base
+	*BaseTask
+	RebalanceId uint64 `xorm:"rebalance_id"`
 }
