8f62dea8361f284477395c2c3dca74c289641ebd
csquan <chengshuangquan@hnnewsoft.com>
2021-12-01 11:09:19 +0800

    add pool info def

--- allocation/main.py
+++ allocation/main.py
@@ -2,7 +2,62 @@
 import requests
 import json
 
-def functionname(url):
+"""对照结果go结构体
+type CrossBalanceItem struct {
+	FromChain    string `json:"from_chain"`
+	ToChain      string `json:"to_chain"`
+	FromAddr     string `json:"from_addr"`
+	ToAddr       string `json:"to_addr"`
+	FromCurrency string `json:"from_currency"`
+	ToCurrency   string `json:"to_currency"`
+	Amount       string `json:"Amount"`
+}
+
+type ReceiveFromBridgeParam struct {
+	ChainId   int
+	ChainName string
+	From      string
+	To        string //合约地址
+
+	Erc20ContractAddr common.Address //erc20 token地址，用于授权
+
+	Amount *big.Int //链上精度值的amount，需要提前转换
+	TaskID *big.Int
+}
+type InvestParam struct {
+	ChainId   int
+	ChainName string
+	From      string
+	To        string //合约地址
+
+	StrategyAddresses  []*common.Address
+	BaseTokenAmount    []*big.Int
+	CounterTokenAmount []*big.Int
+}
+
+type Params struct {
+	CrossBalances           []*CrossBalanceItem       `json:"cross_balances"`
+	ReceiveFromBridgeParams []*ReceiveFromBridgeParam `json:"receive_from_bridge_params"`
+	InvestParams            []*InvestParam            `json:"invest_params"`
+}
+"""
+class Pool:
+    pass
+
+class CrossBalanceItem:
+    pass
+
+class ReceiveFromBridgeParam:
+    pass
+
+class InvestParam:
+    pass
+
+class Params:
+    pass
+
+
+def getprojectinfo(url):
     ret = requests.get(url)
     string = str(ret.content,'utf-8')
     e = json.loads(string)
@@ -16,8 +71,6 @@ def functionname(url):
         for rewardToken in data["rewardTokenList"]:
             prices[rewardToken["tokenSymbol"]] = rewardToken["tokenPrice"]
             dailyReward = float(rewardToken["dayAmount"])*float(rewardToken["tokenPrice"])
-            print("dailyReward is")
-            print(dailyReward)
             reward = reward + dailyReward
     print("totalReward is")
     print(reward)
@@ -33,13 +86,36 @@ def functionname(url):
 
     return prices, reward, tvls, aprs
 
+
+def getpoolinfo(url):
+    ret = requests.get(url)
+    string = str(ret.content,'utf-8')
+    e = json.loads(string)
+    pool_infos = {}
+    for pool_info in e["data"]["pools_info"]:
+        pool = Pool()
+        pool.chain_id = pool_info["chain_id"]
+        pool.symbol = pool_info["symbol"]
+        pool.heco_uncross_quantity = pool_info["heco_uncross_quantity"]
+        pool.bsc_vault_unre_qunatity = pool_info["bsc_vault_unre_qunatity"]
+        pool.poly_vault_unre_qunatity = pool_info["poly_vault_unre_qunatity"]
+        pool.contract_info = pool_info["contract_info"]
+
+        pool_infos[pool_info["chain"]] = pool
+
+    return pool_infos
+
+
 if __name__ == '__main__':
     print("+++++pancake")
     pancakeUrl = 'https://api.schoolbuy.top/hg/v1/project/pool/list?projectId=63'
-    functionname(pancakeUrl)
+    getprojectinfo(pancakeUrl)
     print("+++++biswap")
     biswapUrl = 'https://api.schoolbuy.top/hg/v1/project/pool/list?projectId=476'
-    functionname(biswapUrl)
+    getprojectinfo(biswapUrl)
     print("+++++solo")
     soloUrl = 'https://api.schoolbuy.top/hg/v1/project/pool/list?projectId=63'
-    functionname(soloUrl)
+    getprojectinfo(soloUrl)
+
+    #infoUrl = ''
+    #getpoolinfo(infoUrl)
