059cafb70c3ad663e0809a1dc7212827880e52e8
guoyanbin <guoyanbin@huobi.com>
2021-12-10 18:21:51 +0800

    for removing allocation

--- allocation/utils.py
+++
@@ -1,9 +0,0 @@
-import json
-import decimal
-
-
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, decimal.Decimal):
-            return str(obj)
-        return super(DecimalEncoder, self).default(obj)
