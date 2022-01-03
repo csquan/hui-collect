4a0bcdb3e242241f3e3736f840ec248f275e01f5
wanglei02 <lwightmoon@gmail.com>
2022-01-03 17:18:32 +0800

    check event handled

--- services/part_rebalance/invest_handler.go
+++ services/part_rebalance/invest_handler.go
@@ -1,15 +1,18 @@
 package part_rebalance
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/go-xorm/xorm"
 	"github.com/sirupsen/logrus"
 	"github.com/starslabhq/hermes-rebalance/types"
 	"github.com/starslabhq/hermes-rebalance/utils"
-	"net/http"
 )
 
 type investHandler struct {
-	db types.IDB
+	db       types.IDB
+	eChecker eventChecker
 }
 
 func (i *investHandler) CheckFinished(task *types.PartReBalanceTask) (finished bool, nextState types.PartReBalanceState, err error) {
@@ -33,15 +36,29 @@ func (i *investHandler) CheckFinished(task *types.PartReBalanceTask) (finished b
 }
 
 func (i *investHandler) MoveToNextState(task *types.PartReBalanceTask, nextState types.PartReBalanceState) (err error) {
-	err = utils.CommitWithSession(i.db, func(session *xorm.Session) (execErr error) {
-		task.State = nextState
-		execErr = i.db.UpdatePartReBalanceTask(session, task)
-		if execErr != nil {
-			logrus.Errorf("update part rebalance task error:%v task:[%v]", execErr, task)
-			return
+	var txTasks []*types.TransactionTask
+	var params []*checkEventParam
+	for _, txTask := range txTasks {
+		param := &checkEventParam{
+			chainID: txTask.ChainId,
+			hash:    txTask.Hash,
 		}
-		return
-	})
+		params = append(params, param)
+	}
+	if ok, err1 := i.checkEventsHandled(params); ok && err1 == nil {
+		err = utils.CommitWithSession(i.db, func(session *xorm.Session) (execErr error) {
+			task.State = nextState
+			execErr = i.db.UpdatePartReBalanceTask(session, task)
+			if execErr != nil {
+				logrus.Errorf("update part rebalance task error:%v task:[%v]", execErr, task)
+				return
+			}
+			return
+		})
+	} else {
+		b, _ := json.Marshal(params)
+		logrus.Warnf("event not handled hashs:%s", b)
+	}
 
 	return
 }
@@ -50,20 +67,29 @@ func (i *investHandler) GetOpenedTaskMsg(taskId uint64) string {
 	return ""
 }
 
+func (i *investHandler) checkEventsHandled(params []*checkEventParam) (bool, error) {
+	for _, p := range params {
+		ok, err := i.eChecker.checkEventHandled(p)
+		if err != nil {
+			return false, err
+		}
+		if !ok {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
 type checkEventParam struct {
-	hash string
+	hash    string
 	chainID int
 }
 
 type eventChecker interface {
-	checkEventHandled([]*checkEventParam) (bool, error)
+	checkEventHandled(*checkEventParam) (bool, error)
 }
 
 type eventCheckHandler struct {
 	url string
-	c *http.Client
-}
-
-func(e *eventCheckHandler) checkEventHandled([]*checkEventParam) (bool, error){
-	return true, nil
+	c   *http.Client
 }
