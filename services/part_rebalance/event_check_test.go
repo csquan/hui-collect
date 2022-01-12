44101ba24b9e08e05c9586fe86d9d2459e48abe9
wanglei02 <lwightmoon@gmail.com>
2022-01-12 17:04:31 +0800

    test event check

--- services/part_rebalance/event_check_test.go
+++ services/part_rebalance/event_check_test.go
@@ -11,10 +11,11 @@ func TestInvestEventCheck(t *testing.T) {
 		c:   http.DefaultClient,
 	}
 	ret, err := ie.checkEventHandled(&checkEventParam{
-		Hash:    "0xb3a669d9de51a2a9cd761e52b91cdacd8e2eeef252154ff6ecb9098677dce0ac",
+		Hash:    "0x4665f55638dfa14b788dead36e9ed7890ef60d448cd904670a76ac5ba5f93036",
 		ChainID: 56,
 		StrategyAddrs: []string{
-			"0x7cda523ace086ba0d64eb4a64b4725253627b988",
+			"0xfd8ccf8470da030dabd456de1030698c45352873",
+			"0x21977f1ca61a5d761e3f028f7ec16a4d736bfa31",
 		},
 	})
 	t.Logf("check ret:%v,err:%v", ret, err)
