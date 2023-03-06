22a637f9dd7cbabca43d26ff840912d5012c8702
csquan <314588595@qq.com>
2023-03-06 18:49:23 +0800

    update sql

--- table.sql
+++ table.sql
@@ -67,36 +67,22 @@ CREATE TABLE `t_monitor`
     UNIQUE KEY `uk_addr` (`f_addr`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='监控表';
 
-DROP TABLE IF EXISTS `t_token`;
-create TABLE `t_token` (
-    `f_id`           bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `f_threshold`    varchar(255)        NOT NULL COMMENT '归集门槛',
-    `f_chain`        varchar(255)        NOT NULL COMMENT '链',
-    `f_symbol`       varchar(255)        NOT NULL COMMENT 'token symbol',
-    `f_address`      varchar(255)        NOT NULL COMMENT 'token contract address',
-    `f_decimal`      integer             NOT NULL COMMENT '精度',
-    `f_created_at`   timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'time',
-    `f_updated_at`   timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP COMMENT 'time',
-    PRIMARY KEY (`f_id`) /*T![clustered_index] CLUSTERED */
-)
-ENGINE = InnoDB
-DEFAULT CHARSET = utf8mb4
-COMMENT ='监控币种表';
-
 DROP TABLE IF EXISTS `t_monitor_hash`;
-CREATE TABLE `t_monitor_hash`
-(
-    `f_id`             bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `f_hash`           char(66)            NOT NULL DEFAULT '' COMMENT 'tx hash--交易广播的hash',
-    `f_chain`          char(42)            NOT NULL DEFAULT '' COMMENT '链名称',
-    `f_order_id`            text COMMENT '回调地址',
-    `f_created_at`     timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'time',
-    `f_updated_at`     timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP COMMENT 'time',
-    PRIMARY KEY (`f_id`) /*T![clustered_index] CLUSTERED */,
-    UNIQUE KEY `uk_addr` (`f_hash`,`f_chain`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='监控表';
-
-INSERT INTO t_token (f_id,f_threshold,f_chain,f_symbol,f_address,f_decimal,f_created_at,f_updated_at) VALUES
-            (1,'10000000000000000000','HUI','TSC','0x99Ac689Fd1f09AdA4c0365E6497B2A824Af68557',18,'2023-01-07 10:51:17','2023-01-07 10:56:05'),
-            (2,'10000000000000000000','HUI','TSC1','0xe7df395C170973654A2B054115146f02eE6DfbA5',18,'2023-01-07 10:52:17','2023-01-07 10:56:05'),
-            (3,'10000000000','HUI','TSC111','0x6B98aaa1f8A92ceCA108A49CFe7ee4081B7aF8F8',9,'2023-01-07 10:53:32','2023-01-07 10:56:05');
+CREATE TABLE `t_monitor_hash` (
+      `f_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+      `f_hash` char(66) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT 'tx hash--交易广播的hash',
+      `f_chain` char(42) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '链名称',
+      `f_order_id` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin COMMENT '回调地址',
+      `f_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'time',
+      `f_updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'time',
+      `f_get_receipt_times` int unsigned DEFAULT NULL,
+      `f_receipt_state` int DEFAULT NULL,
+      `f_push_state` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+      `f_contract_addr` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+      `f_gasLimit` int unsigned DEFAULT NULL,
+      `f_gasPrice` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+      `f_gasUsed` int unsigned DEFAULT NULL,
+      `f_index` int DEFAULT NULL,
+      PRIMARY KEY (`f_id`),
+      UNIQUE KEY `uk_addr` (`f_hash`,`f_chain`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='监控表';
