a01de7161f7ba8fa1effdc022712f3512e45f3cf
csquan <314588595@qq.com>
2023-03-24 09:57:46 +0700

    update version for A

--- config/config_test.go
+++ config/config_test.go
@@ -1,12 +1,17 @@
 package config
 
 import (
+	"encoding/hex"
+	"fmt"
 	"testing"
 
 	"gopkg.in/yaml.v3"
 )
 
 func TestLoadConf(t *testing.T) {
+	msg := "436f6e74726163742076616c6964617465206572726f72203a2056616c6964617465205472616e73666572436f6e7472616374206572726f722c2062616c616e6365206973206e6f742073756666696369656e742e"
+	out, _ := hex.DecodeString(msg)
+	fmt.Println(string(out))
 	conf, err := LoadConf("config.yaml")
 	if err != nil {
 		t.Fatalf("load conf err:%v", err)
--- services/collect.go
+++ services/collect.go
@@ -228,7 +228,7 @@ func ToString(any interface{}) string {
 	return "???"
 }
 
-func (c *CollectService) Run() (err error) {
+func (c *CollectService) fun() (err error) {
 	srcTasks, err := c.db.GetOpenedCollectTask()
 	if err != nil {
 		return
